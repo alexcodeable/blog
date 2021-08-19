2b8e11b8c12390fba18f152cd8cf4f876095231c
alexcodeable <alexokomado@gmail.com>
2021-08-19 17:49:10 +0100

    added user show page
    Signed-off-by: alexcodeable <alexokomado@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'all_articles', to: 'articles#all_articles'
   get 'about', to: 'home#about'
   get 'contact', to: 'home#contact'
+  get 'users/:id' => 'users#show'
+  get 'users' => 'users#index'
   get 'dashboard', to: 'dashboard#index'
   resources :articles
   root 'home#index'

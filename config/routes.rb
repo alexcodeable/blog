fc535ad01f79c34b068c69921b41b63603631ab2
alexcodeable <alexokomado@gmail.com>
2021-08-23 10:19:27 +0100

    modified login sign-up routes
    Signed-off-by: alexcodeable <alexokomado@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+    devise_scope :user do
+      get 'sign_up', to: 'devise/registrations#new'
+      post 'sign_up', to: 'devise/registrations#create'
+      get 'login', to: 'devise/sessions#new'
+      post 'login', to: 'devise/sessions#create'
+      delete 'logout', to: 'devise/sessions#destroy'
+      
+    end
   get 'all_articles', to: 'articles#all_articles'
   get 'about', to: 'home#about'
   get 'contact', to: 'home#contact'
-  get 'users/:id' => 'users#show'
-  get 'users' => 'users#index'
+  get 'users/:id', to: 'users#show'
+  get 'users_all', to: 'users#index'
   get 'dashboard', to: 'dashboard#index'
   resources :articles
   root 'home#index'
